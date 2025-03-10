e69363c5b70be3d6b8729241e1e1d8d5e7555078
Romeo Beaurain <beaurainr@Romeos-MacBook-Air.local>
2025-03-10 16:39:25 -0500

    prelaunch branching play

---
+++ components/canvas.tsx
@@ -0,0 +1,11 @@
+const Canvas = () => {
+    return (
+        <div>
+            <h1>Progress</h1>
+            <p>Hello {}, have you completed your habit today?</p>
+            <button>Another day another dollar</button>
+        </div>
+    );
+}
+ 
+export default Canvas;
