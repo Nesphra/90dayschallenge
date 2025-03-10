3db74e15b85264010c44c4ec21432443a70c05a8
Romeo Beaurain <beaurainr@Romeos-MacBook-Air.local>
2025-03-10 18:05:05 -0500

    edited link to post

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 export default async function AuthButton() {
   return (
     <div className="flex gap-2">
-      <Button><a href="https://www.reddit.com/r/NoFap" target="blank_">Request a feature</a></Button>
+      <Button><a href="https://www.reddit.com/r/NoFap/comments/1j8c7oh/90_days_challenge_community_website/" target="blank_">Request a feature</a></Button>
     </div>
   )
 }
