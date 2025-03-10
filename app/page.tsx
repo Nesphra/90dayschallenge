060dcaedc31deb8b2cfb9b7a43c8722947ef4b38
Romeo Beaurain <beaurainr@Romeos-MacBook-Air.local>
2025-03-10 16:43:10 -0500

    main commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default async function Home() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">This is the prelaunch</h2>
+        <h2 className="font-medium text-xl mb-4">Please sign up</h2>
       </main>
     </>
   );
