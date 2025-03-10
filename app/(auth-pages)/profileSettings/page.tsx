0b7b1d1b9d707b253d8d2fee28dc3ab210e77dd5
Romeo Beaurain <beaurainr@Romeos-MacBook-Air.local>
2025-03-10 10:57:43 -0500

    test commit

--- app/(auth-pages)/profileSettings/page.tsx
+++ app/(auth-pages)/profileSettings/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
 import { FormEvent, useEffect, useState } from "react";
 import { createClient } from '@/utils/supabase/client';
 import { useRouter } from 'next/navigation';
@@ -18,32 +18,33 @@ const ChooseUsername = () => {
         getUser();
     }, []);
 
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-        if (!user) return;
+    // const handleSubmit = async (e: FormEvent) => {
+    //     e.preventDefault();
+    //     if (!user) return;
 
-        const formData = new FormData(e.target as HTMLFormElement);
-        const data = Object.fromEntries(formData);
+    //     const formData = new FormData(e.target as HTMLFormElement);
+    //     const data = Object.fromEntries(formData);
         
-        const { error } = await supabase
-            .from('profiles')
-            .update({
-                user_name: data.username,
-            })
-            .eq('id', user.id);
+    //     const { error } = await supabase
+    //         .from('profiles')
+    //         .update({
+    //             user_name: data.username,
+    //         })
+    //         .eq('id', user.id);
 
-        if (!error) {
-            router.push(`/${data.username}`);
-        }
-    }
+    //     if (!error) {
+    //         router.push(`/${data.username}`);
+    //     }
+    // }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <Label htmlFor="username">Enter your username</Label>
-            <Input name="username" placeholder="username" required />
+        <p>profileSettingsPage</p>
+        // <form onSubmit={handleSubmit}>
+        //     <Label htmlFor="username">Enter your username</Label>
+        //     <Input name="username" placeholder="username" required />
     
-            <button type="submit">Save your settings</button>
-        </form>
+        //     <button type="submit">Save your settings</button>
+        // </form>
     );
 }
 
